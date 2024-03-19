3f5c0d53a1feb03e3d3bcd4c4f6b2155ee80e691
GabriDev87 <jgabrielgarciadev@gmail.com>
2024-03-19 20:40:31 +0000

    formatted

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -2,7 +2,7 @@ import Character from "./Character";
 import NotFound from "./NotFound";
 import FilterAndShearchBar from "./FilterAndSearchBar";
 import ReactPaginate from "react-paginate";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 function CharacterList() {
   const [characters, setCharacters] = useState([]);
@@ -54,12 +54,16 @@ function CharacterList() {
               previousLabel={"Prev"}
               nextLabel={"Next"}
               breakLabel={"..."}
-              breakClassName={"flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"}
+              breakClassName={
+                "flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              }
               pageCount={totalPages}
               marginPagesDisplayed={2}
               pageRangeDisplayed={4}
               onPageChange={handleClick}
-              containerClassName={"flex items-center justify-center text-base h-10 p-10 pb-10"}
+              containerClassName={
+                "flex items-center justify-center text-base h-10 p-10 pb-10"
+              }
               previousLinkClassName={
                 "flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
               }
@@ -74,8 +78,6 @@ function CharacterList() {
               }
             />
           </div>
-          
-          
         </div>
       ) : (
         <NotFound />
@@ -83,7 +85,5 @@ function CharacterList() {
     </div>
   );
 }
-
-
 
 export default CharacterList;
