e975f31a530a6f9bea282bea42aeae0d01c16a60
GabriDev87 <jgabrielgarciadev@gmail.com>
2024-03-20 14:32:14 +0000

    First episode and Modal fixed

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -16,9 +16,7 @@ function Character({ character }) {
       location: PropTypes.shape({
         name: PropTypes.string.isRequired,
       }).isRequired,
-      episode: PropTypes.shape({
-        name: PropTypes.string.isRequired,
-      }).isRequired,
+      episode: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
     }).isRequired,
   };
   const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -28,8 +26,8 @@ function Character({ character }) {
     async function fetchData() {
       const response = await fetch(character.episode[0]);
       const data = await response.json();
-      if (data.results && data.results.length > 0) {
-        setFirstEpisode(response.data.name);
+      if (data.results != "") {
+        setFirstEpisode(data.name + " - " + data.episode);
       } else {
         setFirstEpisode("Unknown");
       }
@@ -89,7 +87,7 @@ function Character({ character }) {
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         contentLabel="Character Details"
-        className="flex items-center justify-center outline-none overflow-auto dark:bg-gray-800 rounded-lg px-8 py-6 w-1/3 h-1/3 mx-auto mt-8"
+        className="flex items-center justify-center outline-none overflow-auto dark:bg-gray-800 rounded-lg px-8 py-6 max-w-[500px] max-h-[900px] sm:w-1/2 sm:h-1/2 mx-auto mt-20"
       >
         <div
           className={`w-full max-w-xs bg-white border ${
@@ -111,39 +109,53 @@ function Character({ character }) {
           >
             {character.status}
           </span>
-          <a href="#">
-            <img
-              className="rounded-t-lg w-full"
-              onClick={openModal}
-              src={character.image}
-              alt={character.name}
-              height="300"
-              width="300"
-            />
-          </a>
+          <img
+            className="rounded-t-lg w-full"
+            onClick={openModal}
+            src={character.image}
+            alt={character.name}
+            height="300"
+            width="300"
+          />
+
           <div className="p-5">
-            <a href="#">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {character.name}
-              </h5>
-            </a>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {character.name}
+            </h5>
+
             <p className="font-normal text-gray-700 dark:text-gray-400">
-              Specie: {character.species}
+              Specie:
+            </p>
+            <p className="font-normal text-xl text-gray-700 dark:text-white">
+              {character.species}
             </p>
             <p className="font-normal text-gray-700 dark:text-gray-400">
-              Gender: {character.gender}
+              Gender:
+            </p>
+            <p className="font-normal text-xl text-gray-700 dark:text-white">
+              {character.gender}
             </p>
             <p className="font-normal text-gray-700 dark:text-gray-400">
-              Origin: {character.origin.name}
+              Last known Location:
+            </p>
+            <p className="font-normal text-xl text-gray-700 dark:text-white">
+              {character.location.name}
             </p>
             <p className="font-normal text-gray-700 dark:text-gray-400">
-              Last known Location: {character.location.name}
+              First Episode:
             </p>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              First Episode: {firstEpisode}
+            <p className="font-normal text-xl text-gray-700 dark:text-white pb-5">
+              {firstEpisode}
             </p>
+            <div className="grid justify-items-stretch ">
+              <button
+                className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+                onClick={closeModal}
+              >
+                Close
+              </button>
+            </div>
           </div>
-          <button onClick={closeModal}>Close</button>
         </div>
       </Modal>
     </div>
